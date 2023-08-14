230bfa4b83298c6b35b5e994c1540767a5491ea9
DJDeba <chakradeb321@gmail.com>
2023-08-14 13:46:31 +0530

    baseurl updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:4000/";
+const baseURL = "https://chord-oz5e.onrender.com/";
 
 export const validateUser = async (token) => {
     try{
